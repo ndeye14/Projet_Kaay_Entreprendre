8c6bd48e7d11fc13581d84174244b532a5119bc3
ndeye <ndayawos98@gmail.com>
2023-12-09 15:25:04 +0000

    esssai

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ForumComponent } from './composants/forum/forum.component';
 import { ConditionsGeneralesComponent } from './composants/conditions-generales/conditions-generales.component';
 import { PolitiquesConfidentialitesComponent } from './composants/politiques-confidentialites/politiques-confidentialites.component';
 import { HeaderConnecteComponent } from './composants/header-connecte/header-connecte.component';
+
 import { DomainesComponent } from './composants/domaines/domaines.component';
 import { DashComponent } from './composants/dash/dash.component';
 import { GuidesComponent } from './composants/guides/guides.component';
